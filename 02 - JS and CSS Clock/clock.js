882a2ab79d4eba4ec59539d6a9bcd0b05e24e3dd
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-19 17:07:30 -0500

    autosave script

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -1,19 +1,19 @@
 const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".second-hand");
-const hourHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setTime() {
   const now = new Date();
   const seconds = now.getSeconds();
-  secondsDegrees = (seconds / 60) * 360;
+  secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const minutes = now.getMinutes();
-    minutesDegrees = (seconds / 60) * 360;
-    minuteHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    
-      const hours = now.getSeconds();
-      hoursDegrees = (seconds / 60) * 360;
-      hourHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const minutes = now.getMinutes();
+  minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 setInterval(setTime, 1000);
